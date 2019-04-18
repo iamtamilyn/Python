fe52d3dc684b29cdb7e171a81af8d02a2a9cabec
Tamilyn Peck <tl.tamilyn@gmail.com>
2019-04-17 22:03:01 -0600

    new changes.

---
+++ practiceSpace.py
@@ -0,0 +1,11 @@
+taskTypes = {
+    1: 'Files',
+    2: 'Inquiries',
+    3: 'Emails',
+    4: 'Review',
+    5: 'Call',
+    6: 'Meeting'
+}
+
+
+print(taskTypes)
--- wattApplicationBeta.py
+++ wattApplicationBeta.py
@@ -1,18 +1,30 @@
-from tkinter import *
-from tkinter.ttk import *
+from tkinter import * 
+from tkinter import ttk
+from tkinter import scrolledtext
 import os
 import pyodbc
 import time
 
 window = Tk()
 window.title("Welcome to WATT")
-window.geometry('650x250')
-window.config(background='#C70039')
-# tab 1: entryies
-tab_control = Notebook(window)
-tab1 = Frame(tab_control)
-tab_control.add(tab1, text='Task Entry')
+window.geometry('650x350')
 
+# tab 1: entryies
+tab_control = ttk.Notebook(window)
+tab1 = ttk.Frame(tab_control)
+tab2 = ttk.Frame(tab_control)
+ 
+tab_control.add(tab1, text='   First    ')
+tab_control.add(tab2, text='   Second    ')
+ 
+lbl1 = Label(tab1, text= 'label1')
+lbl1.grid(column=0, row=0)
+ 
+lbl2 = Label(tab2, text= 'label2')
+lbl2.grid(column=0, row=0)
+ 
+tab_control.pack(expand=1, fill='both')
+# tab1.config(background='#C70039')
 # tab 2: Daily WATT - select from today's stuffs
 
 # table for settings, pull in
@@ -61,21 +73,23 @@ def clicked():
 
 # theme combo selected event
 
-clockLabel = Label(text="")
+clockLabel = Label(tab1, text="")
 # clockLabel.pack()
 update_clock()
 
-combo = Combobox(window)
-combo['values']= (1,2,3,4,5,6)
+combo = ttk.Combobox(tab1)
+combo['values']= taskTypes
+# (1,2,3,4,5,6)
 # combo.current(0)
 combo.bind("<<ComboboxSelected>>", selected)
 
-lbl = Label(window,text="Hello, " + username +"!", font=("Arial Bold", 10))
-taskNotelbl = Label(window,text="Task Note", font=("Arial", 8))
-clientCodelbl = Label(window,text="Code", font=("Arial", 8))
-taskNoteTxt = Entry(window,width=20)
-clientCodeTxt = Entry(window,width=20)
-btn = Button(window, text="Start Task", command=clicked, width=40)
+# Tab 1 Objects
+lbl = Label(tab1,text="Hello, " + username +"!", font=("Arial Bold", 10))
+taskNotelbl = Label(tab1,text="Task Note", font=("Arial", 8))
+clientCodelbl = Label(tab1,text="Code", font=("Arial", 8))
+taskNoteTxt = Entry(tab1,width=20)
+clientCodeTxt = Entry(tab1,width=20)
+btn = Button(tab1, text="Start Task", command=clicked, width=25)
 
 lbl.grid(column=0, row=0)
 # Label Row 1
@@ -89,6 +103,10 @@ taskNoteTxt.grid(column=2,row=2,padx=(5, 5))
 btn.grid(column=3, row=2,padx=(5, 5))
 
 clockLabel.grid(column=2,row=3)
+
+# Tab 2 Objects
+txtBox = scrolledtext.ScrolledText(tab2,width=40,height=10)
+txtBox.grid(column=0,row=0)
 # Current Queue/Duration Row
 
 
